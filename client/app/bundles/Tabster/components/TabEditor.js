161b03ef8f18266afc80121ae8d07331f31617a8
Matt Swieboda <matt.swieboda@centro.net>
2017-07-10 18:56:20 -0500

    WIP guitar fretboard, forgot to add pg shard

---
+++ client/app/bundles/Tabster/components/TabEditor.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+let TabEditor = () => {
+  return (
+    <div className="fretboard-container">
+      <div className="string-labels">
+        <div className="label">
+          <div>e</div>
+        </div>
+        <div className="label">
+          <div>B</div>
+        </div>
+        <div className="label">
+          <div>G</div>
+        </div>
+        <div className="label">
+          <div>D</div>
+        </div>
+        <div className="label">
+          <div>A</div>
+        </div>
+        <div className="label">
+          <div>E</div>
+        </div>
+      </div>
+
+      <div className="fretboard">
+        <div className="string">
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+        <div className="string">
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+        <div className="string">
+
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+        <div className="string">
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+        <div className="string">
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+        <div className="string">
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+          <span className="fret">&nbsp;</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TabEditor;
