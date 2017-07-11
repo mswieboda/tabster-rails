2ed969a42c9f1ae718b89e4a0ac9bef0b1c3a95f
Matt Swieboda <mswiebod@gmail.com>
2017-07-10 19:55:52 -0500

    add create tab form and route

--- client/app/bundles/Tabster/containers/AddTab.js
+++ client/app/bundles/Tabster/containers/AddTab.js
@@ -8,17 +8,18 @@ let AddTab = ({ dispatch }) => {
 
   return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTab(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
+      <form action="/api/tabs/create" method="post">
+
+        <label htmlFor="title">Title</label>
+        <div>
+          <input name="title" id="title" />
+        </div>
+
+        <label htmlFor="tab">Tab</label>
+        <div>
+          <textarea name="tab"  id="tab"></textarea>
+        </div>
+
         <button type="submit">
           Add Tab
         </button>
--- src/routes/web.cr
+++ src/routes/web.cr
@@ -1,15 +1,23 @@
 # Matches GET "http://host:port/"
 get "/" do
-  Tabster.tabs_index
+  TabsController.index
+end
+
+post "/api/tabs/create" do |env|
+  TabsController.create(env.params.body.to_h)
 end
 
 get "/*path" do
-  Tabster.tabs_index
+  TabsController.index
 end
 
-module Tabster
-  def self.tabs_index
+module TabsController
+  def self.index
     props = { tabs: Tab.all.to_a.map { |t| {title: t.title, tab: t.tab} } }
     render "src/views/tabster/app.ecr", "src/views/layouts/main.ecr"
+  end
+
+  def self.create(tab_params)
+    Tab.create!(tab_params)
   end
 end
