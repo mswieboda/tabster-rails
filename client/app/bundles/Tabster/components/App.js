2d90a90a4ae79544c1769799dc69338d49b93de6
Matt Swieboda <mswiebod@gmail.com>
2017-07-10 20:59:32 -0500

    guitar fretboard work

--- client/app/bundles/Tabster/components/App.js
+++ client/app/bundles/Tabster/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Tabs from './Tabs'
+import TabEditor from './TabEditor'
 
 const App = ({match}) => (
   <div>
     <Route path="/" render={() => (
-      <Link to="/tabs">tabs</Link>
+      <div>
+        <TabEditor />
+        <Link to="/tabs">tabs</Link>
+      </div>
     )}/>
     <Route path="/tabs" component={Tabs} />
   </div>
--- client/app/bundles/Tabster/components/TabEditor.js
+++ client/app/bundles/Tabster/components/TabEditor.js
@@ -1,104 +1,68 @@
+const cn = require('classnames');
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const strings = ['e', 'B', 'G', 'D', 'A', 'E'];
+const frets = [
+  { n: 1 },
+  { n: 2 },
+  { n: 3, dot: true },
+  { n: 4 },
+  { n: 5, dot: true },
+  { n: 6 },
+  { n: 7, dot: true },
+  { n: 8 },
+  { n: 9, dot: true },
+  { n: 10 },
+  { n: 11 },
+  { n: 12, dot: true },
+  { n: 13 },
+  { n: 14, dot: true },
+  { n: 15 },
+  { n: 16, dot: true },
+  { n: 17 },
+];
+const dots = [3, 5, 7, 9, 15, 17, 19, 21];
+const doubleDots = [12, 24];
+
+let StringLabel = ({ label }) => (
+  <div className="label">{label}</div>
+);
+
+let StringRow = ({ string, frets }) => {
+  return (
+    <div className="string">
+      {frets.map(fret =>
+        <Fret
+          key={`s${string}-f${fret.n}`}
+          fret={fret}
+          dot
+          doubleDot
+        />
+      )}
+    </div>
+  );
+};
+
+let Fret = ({ n }, { dot }, { doubleDot }) => {
+  const classes = cn({
+    fret: true,
+    dot: dot,
+    'double-dot': doubleDot
+  });
+
+  return (<span className={classes}>&nbsp;</span>);
+};
 
 let TabEditor = () => {
   return (
     <div className="fretboard-container">
       <div className="string-labels">
-        <div className="label">
-          <div>e</div>
-        </div>
-        <div className="label">
-          <div>B</div>
-        </div>
-        <div className="label">
-          <div>G</div>
-        </div>
-        <div className="label">
-          <div>D</div>
-        </div>
-        <div className="label">
-          <div>A</div>
-        </div>
-        <div className="label">
-          <div>E</div>
-        </div>
+        {strings.map(string => <StringLabel label={string} key={`s${string}-label`} />)}
       </div>
 
       <div className="fretboard">
-        <div className="string">
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
-        <div className="string">
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
-        <div className="string">
-
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
-        <div className="string">
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
-        <div className="string">
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
-        <div className="string">
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-          <span className="fret">&nbsp;</span>
-        </div>
+        {strings.map(string => <StringRow string={string} frets={frets} key={`s${string}`} />)}
       </div>
     </div>
   );
