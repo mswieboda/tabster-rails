31663e90d907edf990c90f17ec0b8afdb7d372d6
Matt Swieboda <mswiebod@gmail.com>
2017-11-28 16:17:39 -0600

    switch to Rails 5.1.4 with webpack=react Xing Fingers

--- app/controllers/tabs_controller.rb
+++
@@ -1,7 +0,0 @@
-class TabsController < ApplicationController
-  def index
-    @props = {
-      tabs: JSON.parse(Tab.all.to_json)
-    }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  root 'tabs#index'
-  get '*path', to: 'tabs#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
